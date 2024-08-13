a018c900c582233a1a67c519f6fdcef0e1f6f866
Logesh Suresh <logeshsrinivasans@gmail.com>
2024-08-13 23:18:19 +0530

    Added application lifecycle hooks

--- src/index.js
+++ src/index.js
@@ -4,11 +4,23 @@ const fastify = require("fastify")({
 
 const PORT = 3000;
 
+// Application lifecycle -> `onReady` Acts when app is ready to start instance
+fastify.addHook("onReady", function listener(done) {
+    console.log("Server starting up.....READY TO TAKE REQUESTS");
+    done();
+});
+
+// Application lifecycle -> `onClose` Acts when app is stopping
+fastify.addHook("onClose", function listener(_) {
+    console.log("Server shutting down....");
+});
+
 async function start() {
     try {
         await fastify.listen({ port: 3000 });
         console.log(`Server running on PORT: ${PORT}`);
     } catch (error) {   
+        fastify.close();
         console.log(error);
         process.exit(1);
     }
